a42ac4096f6a14f6c094b42d21ab54a9cf8bc5d2
galaxey56 <satyakiran56@gmail.com>
2022-11-14 10:16:48 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "../styles/App.css";
 import useLocalStorage from "./useLocalStorage";
 
 const App = () => {
+  const [state, setState] = useLocalStorage("key", null);
+  const inputRef = useRef(null);
 
   const submitHandler = (e) => {
     e.preventDefault();
-
-    
-
+    const text = inputRef.current.value;
+    setState(text);
   };
 
   return (
     <div id="main">
       <label>Enter a Value to save in localStorage</label>
-      <input type="text" id="value"  />
-      <button id='set' onClick={submitHandler}>Set Value</button>
+      <input type="text" id="value" ref={inputRef} />
+      <button id="set" onClick={submitHandler}>
+        Set Value
+      </button>
     </div>
   );
 };
